2329cc44bde51622e06ee5152572baab6eb7bd97
michel <lug.guibolo@yaoo.fr>
2012-01-03 15:39:36 +0100

    first commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+admin:ee10c315eba2c75b403ea99136f5b48d:admin:admin@gmail.com:admin,user
